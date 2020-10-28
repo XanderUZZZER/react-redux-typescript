4f5836ae958c6c7e8e4f48d86928cb46c88f3f15
xanderuzzzer <xanderuzzzer@gmail.com>
2020-10-28 17:58:39 +0200

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
+import { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 interface AppProps {
-  color: string
+  color?: string
 }
 
-const App = (props: AppProps) => {
-  return <div>{props.color}</div>
+interface AppState {
+  counter: number
+}
+
+const App: React.FC<AppProps> = () => {
+  const [{ counter }, setCounter] = useState<AppState>(0)
+
+  return (
+    <div>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <button onClick={() => setCounter(counter - 1)}>-</button>
+      <p>{counter}</p>
+    </div>
+  )
 }
 
 ReactDOM.render(<App color='red' />, document.querySelector('#root'))
