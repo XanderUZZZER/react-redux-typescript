d14b27edac7231369b83349e1a5ee68bb0884ff0
xanderuzzzer <xanderuzzzer@gmail.com>
2020-10-29 13:40:38 +0200

    Reducer with enums

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import { App } from './components/App'
+
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+
+import { reducers } from './reducers'
 
 const store = createStore(reducers, applyMiddleware(thunk))
 
