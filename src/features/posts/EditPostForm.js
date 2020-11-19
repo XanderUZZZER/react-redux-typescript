8bce57cfd45875a86e043a41b2afa0fc804097af
XanderUZZZZER <xanderuzzzer@gmail.com>
2020-11-19 23:08:24 +0200

    EditPost route added

--- src/features/posts/EditPostForm.js
+++ src/features/posts/EditPostForm.js
@@ -1,10 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import { postUpdated } from './postsSlice'
 
+export const EditPostForm = ({ match }) => {
+  const { postId } = match.params
+  const post = useSelector(state => state.posts.find(post => post.id === postId))
 
-export const EditPostForm = () => {
+  const [title, setTitle] = useState(post.title);
+  const [content, setContent] = useState(post.content);
+
+  const dispatch = useDispatch()
+  const history = useHistory()
+
+  const onTitleChanged = e => setTitle(e.target.value)
+  const onContentChanged = e => setContent(e.target.value)
+
+  const onSavePostClicked = () => {
+    if (title && content) {
+      dispatch(postUpdated({ id: postId, title, content }))
+      history.push(`/posts/${postId}`)
+    }
+  }
+
   return (
-    <div>
-      Edit post form
-    </div>
+    <form>
+      <h2>Edit Post</h2>
+
+      <div className="form-group">
+        <label htmlFor="postTitle">Post title</label>
+        <input
+          type="text"
+          name="postTitle"
+          id="postTitle"
+          className="form-control"
+          value={title}
+          onChange={onTitleChanged}
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="postContent">Post content:</label>
+        <textarea
+          name="postContent"
+          id="postContent"
+          className="form-control"
+          value={content}
+          onChange={onContentChanged}
+        />
+      </div>
+
+      <div className='d-flex'>
+        <button
+          type="button"
+          className="btn btn-primary ml-auto"
+          onClick={onSavePostClicked}
+        >
+          Save Post
+        </button>
+      </div>
+    </form>
   )
 }
--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 export const SinglePostPage = ({ match }) => {
   const { postId } = match.params
@@ -22,6 +23,12 @@ export const SinglePostPage = ({ match }) => {
         <h2 className='display-4'>{post.title}</h2>
         <hr />
         <p className="lead">{post.content}</p>
+        <Link
+          to={`/editPost/${postId}`}
+          className="btn btn-primary ml-auto"
+        >
+          Edit Post
+        </Link>
       </article>
     </section>
   )
