db46ed40f41da716ad71ded485a73939a5eccf53
XanderUZZZZER <xanderuzzzer@gmail.com>
2020-11-23 22:06:19 +0200

    Loading Users

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { client } from '../../api/client'
 
 const initialState = [
   { id: '0', name: 'Tianna Jenkins' },
@@ -6,10 +7,19 @@ const initialState = [
   { id: '2', name: 'Madison Price' }
 ]
 
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  const response = await client.get('fakeApi/users')
+  return response.users
+})
+
 const usersSlice = createSlice({
   name: 'users',
   initialState,
-  reducers: {
+  reducers: {},
+  extraReducers: {
+    [fetchUsers.fulfilled]: (state, action) => {
+      return action.payload
+    }
   }
 })
 
