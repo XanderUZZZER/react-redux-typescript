aa55237a218a29f4722299e14baef1659ec856ca
XanderUZZZZER <xanderuzzzer@gmail.com>
2020-11-23 20:16:48 +0200

    Loading State for Requests

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -2,36 +2,40 @@ import { createSlice } from '@reduxjs/toolkit'
 import { nanoid } from '@reduxjs/toolkit'
 import { sub } from 'date-fns'
 
-const initialState = [
-  {
-    id: nanoid(),
-    date: sub(new Date(), { minutes: 10 }).toISOString(),
-    title: 'First post',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, sed.',
-    reactions: {
-      thumbsUp: 0,
-      hooray: 0,
-      heart: 0,
-      rocket: 0,
-      eyes: 0
+const initialState = {
+  posts: [
+    {
+      id: nanoid(),
+      date: sub(new Date(), { minutes: 10 }).toISOString(),
+      title: 'First post',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, sed.',
+      reactions: {
+        thumbsUp: 0,
+        hooray: 0,
+        heart: 0,
+        rocket: 0,
+        eyes: 0
+      },
+      user: '2'
     },
-    user: '2'
-  },
-  {
-    id: nanoid(),
-    date: sub(new Date(), { minutes: 5 }).toISOString(),
-    title: 'Second post',
-    content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium ut aliquam quas aliquid sequi. Excepturi quas voluptatum enim quibusdam illum.',
-    reactions: {
-      thumbsUp: 0,
-      hooray: 0,
-      heart: 0,
-      rocket: 0,
-      eyes: 0
-    },
-    user: '0'
-  }
-]
+    {
+      id: nanoid(),
+      date: sub(new Date(), { minutes: 5 }).toISOString(),
+      title: 'Second post',
+      content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium ut aliquam quas aliquid sequi. Excepturi quas voluptatum enim quibusdam illum.',
+      reactions: {
+        thumbsUp: 0,
+        hooray: 0,
+        heart: 0,
+        rocket: 0,
+        eyes: 0
+      },
+      user: '0'
+    }
+  ],
+  status: 'idle',
+  error: null
+}
 
 const postsSlice = createSlice({
   name: 'posts',
@@ -39,7 +43,7 @@ const postsSlice = createSlice({
   reducers: {
     postAdded: {
       reducer(state, action) {
-        state.push(action.payload)
+        state.posts.push(action.payload)
       },
       prepare(title, content, userId) {
         return {
@@ -62,7 +66,7 @@ const postsSlice = createSlice({
     },
     postUpdated(state, action) {
       const { id, title, content } = action.payload
-      const existingPost = state.find(post => post.id === id)
+      const existingPost = state.posts.find(post => post.id === id)
 
       if (existingPost) {
         existingPost.title = title
@@ -71,7 +75,7 @@ const postsSlice = createSlice({
     },
     reactionAdded(state, action) {
       const { postId, reaction } = action.payload
-      const existingPost = state.find(post => post.id === postId)
+      const existingPost = state.posts.find(post => post.id === postId)
 
       if (existingPost) {
         existingPost.reactions[reaction]++
@@ -84,6 +88,6 @@ export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
 
 export default postsSlice.reducer
 
-export const selectAllPosts = state => state.posts
+export const selectAllPosts = state => state.posts.posts
 
-export const selectPostById = (state, postId) => state.posts.find(post => post.id === postId)
+export const selectPostById = (state, postId) => state.posts.posts.find(post => post.id === postId)
