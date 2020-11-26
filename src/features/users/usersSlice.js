471eb9612f7ecc738a6b4b3be81f5a849a511f44
XanderUZZZZER <xanderuzzzer@gmail.com>
2020-11-26 22:37:47 +0200

    Converting the Users Slice

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,11 +1,9 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk, createEntityAdapter } from '@reduxjs/toolkit'
 import { client } from '../../api/client'
 
-const initialState = [
-  // { id: '0', name: 'Tianna Jenkins' },
-  // { id: '1', name: 'Kevin Grant' },
-  // { id: '2', name: 'Madison Price' }
-]
+const userAdapter = createEntityAdapter()
+
+const initialState = userAdapter.getInitialState()
 
 export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
   const response = await client.get('/fakeApi/users')
@@ -17,14 +15,13 @@ const usersSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: {
-    [fetchUsers.fulfilled]: (state, action) => {
-      return action.payload
-    }
+    [fetchUsers.fulfilled]: userAdapter.setAll
   }
 })
 
 export default usersSlice.reducer
 
-export const selectAllUsers = state => state.users
-
-export const selectUserById = (state, userId) => state.users.find(user => user.id === userId)
+export const {
+  selectAll: selectAllUsers,
+  selectById: selectUserById
+} = userAdapter.getSelectors(state => state.users)
